0a1f78b7b7c0627935218d23d0f10522b3bc9238
oemuraye <oemuraye@gmail.com>
2024-06-29 21:33:54 +0100

    vercel deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import App from './App';
+import './index.css';
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.min.css";
